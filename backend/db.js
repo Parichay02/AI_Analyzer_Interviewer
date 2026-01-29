312d6b83ebfe5f3a282d20cda46d0e87f7339514
Parichay02 <130536873+Parichay02@users.noreply.github.com>
2026-01-29 18:35:27 +0530

    Mask database password in db.js

--- backend/db.js
+++ backend/db.js
@@ -3,7 +3,7 @@ import mysql from "mysql2/promise";
  const db = mysql.createPool({
   host: "mysql",       // MySQL service name
   user: "appuser",     // must match MYSQL_USER
-  password: "14800121045", // must match MYSQL_PASSWORD
+  password: "**********", // must match MYSQL_PASSWORD
   database: "authdb", // must match MYSQL_DATABASE
   waitForConnections: true,
   connectionLimit: 10,
