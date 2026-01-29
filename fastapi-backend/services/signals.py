ab45bc61f18911d0ffa25b5f55952a0392e713b7
Parichay02 <130536873+Parichay02@users.noreply.github.com>
2026-01-29 19:51:09 +0530

    Update OpenAI API key in signals.py
    Replaced the OpenAI API key with a placeholder.

--- fastapi-backend/services/signals.py
+++ fastapi-backend/services/signals.py
@@ -4,7 +4,7 @@ import json
 
 llm = ChatOpenAI(
     model="gpt-4o-mini",
-    openai_api_key="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" ,
+    openai_api_key="xxxx"
     temperature=0
 )
 
